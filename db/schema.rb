774920e98afac0a471e9fe704bec15f93dae888e
Blarglenarky <blarglenarky@gmail.com>
2026-09-14 14:08:44 -0500

    Fixed errors and improved newly impemented book features

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_12_015305) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_14_190347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    #added these into the database schema
-    t.string "author"
-    t.decimal "price", precision: 10, scale: 2
-    t.date "published_date"
-    
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.decimal "price"
+    t.date "published_date"
   end
 end
