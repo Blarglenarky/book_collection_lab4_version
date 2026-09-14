24578e58df69c6cba2ba094a88486f2935f1512c
Blarglenarky <blarglenarky@gmail.com>
2026-09-14 13:58:20 -0500

    Added functionality for author, price, and published_date to the Book model and updated the database schema accordingly. Also added RSpec for testing.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_12_015305) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    #added these into the database schema
+    t.string "author"
+    t.decimal "price", precision: 10, scale: 2
+    t.date "published_date"
+    
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe Book, type: :model do
             expect(book).to_not be_valid
         end
     end
+
+    describe "author" do
+        it "accepts an author" do
+            book = Book.new(title: "Name testing book", author: "Author Name")
+            expect(book).to be_valid
+        end
+
+    end
+
+    describe "price" do
+        it "accepts a price" do
+            book = Book.new(title: "Name testing book", price: 19.99)
+            expect(book).to be_valid
+        end
+    end
+
+    describe "published date" do
+        it "accepts a published date" do
+            published_date = Date.new(2026, 9, 14)
+            book = Book.new(title: "Name testing book", published_date: published_date)
+            expect(book).to be_valid
+        end
+    end
 end
